f5c888367c780623535ae6fa80418dc6bf2dd48a
agil20 <100156146+agil20@users.noreply.github.com>
2022-09-06 09:30:01 -0700

    feat:save change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
-// const burgerright=document.getElementsByClassName("burger_right");
-// burgerright.addEventListener('click', () => {
-//    alert('Please select')
-//   });
+const menuBtn = document.querySelector('.menu-btn');
+let count=0;
+const closeright =document.querySelector('.close_right');
+const burgerright =document.querySelector('.burger_right');
+let menyubugers=document.querySelector(".men");
+menuBtn.addEventListener('click', (e) => {
+   count++; 
+  
+ if (count%2==1)
+ {
+    closeright.classList.remove('hidden_menu');
+
+    burgerright.classList.add('hidden_menu');
+  //  menyubugers.classList.remove('invisible');
+
+ }
+ else
+ {
+    closeright.classList.add('hidden_menu');
+    burgerright.classList.remove('hidden_menu');
+
+ }
+ 
+  
+ 
+
+  });
