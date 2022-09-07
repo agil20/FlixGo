467c154a001dcc13328ea322f230b447b3fc2664
agil20 <100156146+agil20@users.noreply.github.com>
2022-09-07 10:19:32 -0700

    feat: slder formatini qurdum

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,28 @@ let count=0;
 const closeright =document.querySelector('.close_right');
 const burgerright =document.querySelector('.burger_right');
 let menyubugers=document.querySelector(".menyu-bugers");
+let searchicon = document.querySelector('.search_icon');
+let searchcontainer=document.querySelector('.search_container');
+
+let openemu = false;
+searchicon.addEventListener('click',() => {
+   if(!openemu) {
+      searchcontainer.style.visibility = "visible";
+      searchcontainer.style.transition = "duration(0s)" 
+      searchcontainer.style.transform = "translatey(0px)"
+      searchcontainer.style.opacity="1" 
+      
+      openemu = true;
+   }
+   else {
+      searchcontainer.style.visibility="hidden";
+      searchcontainer.style.transition = "duration(1s)" 
+      searchcontainer.style.opacity="0" 
+      searchcontainer.style.transform = "translatey(-50px)"
+      openemu = false;
+   }
+});
+
 menuBtn.addEventListener('click', (e) => {
    count++; 
   
@@ -23,7 +45,26 @@ menuBtn.addEventListener('click', (e) => {
     menyubugers.classList.add('d-none');
  }
  
-  
+  });
+  $(document).ready(function(){
+   $('.owl-carousel').owlCarousel({
+       loop:true,
+       margin:10,
+       nav:true,
+       center: true,
+       responsive:{
+           0:{
+               items:1
+           },
+           600:{
+               items:3
+           },
+           1000:{
+               items:5
+           }
+       }
+   });
+ });
  
 
-  });
+  
