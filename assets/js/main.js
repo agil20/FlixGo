4e4ceb679d641914c8d042cd95344855ecea1f2e
agil20 <100156146+agil20@users.noreply.github.com>
2022-09-07 03:22:45 -0700

    feat: searche input elave eledim

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,26 @@
+
 const menuBtn = document.querySelector('.menu-btn');
 let count=0;
 const closeright =document.querySelector('.close_right');
 const burgerright =document.querySelector('.burger_right');
-let menyubugers=document.querySelector(".men");
+let menyubugers=document.querySelector(".menyu-bugers");
 menuBtn.addEventListener('click', (e) => {
    count++; 
   
  if (count%2==1)
  {
     closeright.classList.remove('hidden_menu');
-
+   
     burgerright.classList.add('hidden_menu');
-  //  menyubugers.classList.remove('invisible');
+    menyubugers.classList.remove('d-none');
+
 
  }
  else
  {
     closeright.classList.add('hidden_menu');
     burgerright.classList.remove('hidden_menu');
-
+    menyubugers.classList.add('d-none');
  }
  
   
